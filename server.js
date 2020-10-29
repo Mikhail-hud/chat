7a86f671c50fa5d58e34b0176a57e704149962e6
Mikhail-hud <mykhaylohud@gmail.com>
2020-10-29 19:16:33 +0200

    make it better

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 
-
 const express = require("express");
+const cors = require('cors');
 
 const app = express();
 const server = require("http").Server(app);
 const io = require("socket.io")(server);
 
 app.use(express.json());
+app.use(cors());
 
 const rooms = new Map();
 
